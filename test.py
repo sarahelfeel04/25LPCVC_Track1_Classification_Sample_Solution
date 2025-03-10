ea36bc4798bf470ba015e3bad78246427f13272b
Sarah <sarah.elfeel@gmail.com>
2025-03-10 20:16:08 +0200

    adjust dataset upload and model inference

---
+++ test.py
@@ -0,0 +1,12 @@
+import qai_hub
+import numpy as np
+
+dataset_id = "dv74qmp02"
+model_id = "mn4xdzprq"  # Your compiled model ID
+
+# Debugging: Check dataset info
+dataset = qai_hub.get_dataset(dataset_id)
+print("Dataset Info:", dataset)
+
+
+
